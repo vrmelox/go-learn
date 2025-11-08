4e1686c39461d5bbc789f649cfd59ec330cbf8e0
vrmelo <akandeabiodoun@gmail.com>
2025-11-08 08:57:50 +0100

    I've enhanced the functions and test it

--- setting_up/check_square.go
+++ setting_up/check_square.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 
 func verifySquare(mapi []string, row int, col int, size int) int {
-	i := row
-	j := col
-	for ; i < i + size ; i++ {
-		for ; j  < j + size ; j++ {
+	limiti := row + size
+	limitj := col + size
+
+	for i := row; i < limiti ; i++ {
+		for j := col; j  < limitj ; j++ {
 			if mapi[i][j] != '.' {
 				return 0
 			}
@@ -16,3 +20,23 @@ func verifySquare(mapi []string, row int, col int, size int) int {
 	return 1
 }
 
+// func checksquare(mapi []string, row int, col int, size int) int {
+// 	cuti := mapi[row : row + size]
+// 	for _, elem := range cuti {
+// 		subs := elem[col : col + size]
+// 		if strings.Contains(subs, "o") {
+// 			return 0
+// 		}
+// 	}
+// 	return 1
+// }
+
+func main () {
+	file := os.Args[1]
+	mapi, err := getContent(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	re := verifySquare(mapi, 5, 3, 4)
+	fmt.Println(re)
+}
